71d7c620c1f63086f73fbda4b05117cf3c9873af
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-07-25 18:39:21 +0200

    add google authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations", omniauth_callbacks: 'callbacks' }
+    devise_scope :user do 
+      get 'login', to: 'devise/sessions#new'
+    end
+    devise_scope :user do 
+      get 'signup', to: 'devise/registrations#new'
+    end
   root 'pages#home'
 end
